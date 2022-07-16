d3e7185c67e79bd44ed43622a348472a5f3cbedf
William sarti josé <william.jose@totvs.com.br>
2022-07-16 18:59:31 -0300

    Exercicio- Vetores - Parte 1

---
+++ src/ExemploVetor/VetoresParte1.java
@@ -0,0 +1,37 @@
+package ExemploVetor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VetoresParte1 {
+
+//	Fazer um programa para ler um número inteiro N e a altura de N
+//	pessoas. Armazene as N alturas em um vetor. Em seguida, mostrar a
+//	altura média dessas pessoas.
+	
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		double [] vect = new double [N];
+		
+		for(int i = 0; i < N; i++) {
+			vect[i] = sc.nextDouble();	
+		}
+		
+		double sum = 0.0;
+		
+		for(int i = 0; i < N; i++) {
+			sum += vect[i];
+		}
+		double avg = sum / N ;
+		
+		System.out.printf("AVERAGE HEIGHT: %2f%n", avg);
+		sc.close();
+
+	}
+
+}
