209d1b68d5d3714597293aeb963f6547f7cd3e2d
William sarti josé <william.jose@totvs.com.br>
2022-07-30 00:49:24 -0300

    Problema soma_vetor

---
+++ src/soma_vetor.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class soma_vetor {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+
+		int n;
+		double soma, media;
+
+		System.out.println("Quantos numeros você vai digitar ? ");
+		n = sc.nextInt();
+
+		double[] vetor = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Digite um numero: ");
+			vetor[i] = sc.nextDouble();
+		}
+
+		soma = 0;
+
+		for (int i = 0; i < n; i++) {
+			soma = soma + vetor[i];
+		}
+
+		media = soma / n;
+
+		System.out.println("Valores = ");
+
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%.1f  ", vetor[i]);
+		}
+
+		System.out.printf("\nSOMA = %.2f\n", soma);
+		System.out.printf("MEDIA = %.2f\n", media);
+
+		sc.close();
+	}
+}
